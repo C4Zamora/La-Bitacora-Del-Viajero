b678997dd6ebf3a6e300043cfbcbf583dfbcb13e
C4Zamora <caroldzamora4@gmail.com>
2024-11-09 20:19:51 -0500

    Registrate

--- src/componentes/Registro.jsx
+++ src/componentes/Registro.jsx
@@ -1,11 +1,50 @@
 import React from "react";
-import '../componentes/Login.css'
+import '../componentes/Registro.css'
 
 export const Registro = () => {
   return (
-    <main>
-        <h1>Registro</h1>
+          <main>
      
+       <div className="registro-container">
+      {/* Lado izquierdo: formulario de registro */}
+      <div className="form-registro-container">
+        
+        <form action="#" method="POST">
+          <h1>Registrate</h1>
+  <div className="form-group">
+    <input
+      type="text"
+      id="username"
+      name="username"
+      placeholder="Nombre de usuario"
+      required
+    />
+  </div>
+  <div className="form-group">
+    <input
+      type="email"
+      id="email"
+      name="password"
+      placeholder="Correo"
+      required
+    /></div>
+  <div className="form-group">
+    <input
+      type="password"
+      id="password"
+      name="password"
+      placeholder="Contraseña"
+      required
+    />
+  </div>
+  <button className="button-form" type="submit">Registrar</button>
+  <div className="registrate-container">
+      
+  </div>
+</form>
+</div>
+</div>
+    
     
     </main>
   )

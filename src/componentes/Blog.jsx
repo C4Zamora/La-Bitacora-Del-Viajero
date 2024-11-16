ab2ed172c49516c8a04681414bc461e89e740f33
C4Zamora <caroldzamora4@gmail.com>
2024-11-15 22:34:39 -0500

    Blog

--- src/componentes/Blog.jsx
+++ src/componentes/Blog.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import '../componentes/Blog.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export const Blog = () => {
     const urlApi = "https://6622071827fcd16fa6c8818c.mockapi.io/api/v1";
@@ -73,7 +75,7 @@ export const Blog = () => {
       </nav>
       <div className="banner">
         <div>
-          <img src="/src/img/Lugar1.jpg" alt="Slide 1" />
+          <img src="/src/img/banner.jpg" alt="Slide 1" />
         </div>
         <div className="botones-select">
           <Link to="/MisPost"><button className="boton-select1">Mis Post</button></Link>
@@ -84,14 +86,16 @@ export const Blog = () => {
       <div className="new-post-container">
         <h2>Nuevo Post</h2>
         <h3>{user.name}</h3>
-        <h3>Tienes {posts.length} posts publicados</h3>
+        <h4>Tienes {posts.length} Post publicados</h4>
+
         <form onSubmit={onSubmit}>
-          <input type="text" placeholder="Nombre" name="name" onChange={onChange} />
+          <input type="text" placeholder="Lugar" name="name" onChange={onChange} />
           <input type="text" placeholder="Ubicación" name="location" onChange={onChange} />
           <input type="text" placeholder="URL de la imagen" name="imageUrl" onChange={onChange} />
           <textarea placeholder="Reseña" name="review" onChange={onChange}></textarea>
           <button type="submit">Publicar</button>
         </form>
+        </div>
 
         <div className="posts-container">
           {posts.map((post) =>
@@ -106,9 +110,17 @@ export const Blog = () => {
             ) : (
               <div className="post" key={post.id}>
                 <div className="actions">
-                  <div className="action" onClick={() => deletePost(post.id)}></div>
-                  <div className="action" onClick={() => editPost(post.id)}></div>
+                  
+          
+                
+                  <div className="action1"  onClick={() => deletePost(post.id)}>
+                  <i className="fas fa-trash-alt"></i>
+                  </div>
+                  <div className="action2" onClick={() => editPost(post.id)}>
+                  <i className="fas fa-edit"></i>
+                  </div>
                 </div>
+
                 <h4>{post.name}</h4>
                 <p>{post.review}</p>
                 <p><strong>Ubicación:</strong> {post.location}</p>
@@ -117,7 +129,7 @@ export const Blog = () => {
             )
           )}
         </div>
-      </div>
+      
     </main>
   );
 };
